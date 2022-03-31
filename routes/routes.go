fb0bf766143cffd9e50a83a44878b4357ad0fe9f
Teten Nugraha <teten.nugraha18@gmail.com>
2022-03-31 10:07:12 +0700

    sign up routes

---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/teten-nugraha/go-dev-forum/controller"
+)
+
+func SetupRoutes(server *fiber.App) {
+
+	api := server.Group("/api/v1")
+
+	api.Post("/signUp", controller.Register)
+}
