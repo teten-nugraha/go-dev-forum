864e95cd52357e17c587f8b09a1d84e4aa4b7e4a
Teten Nugraha <teten.nugraha18@gmail.com>
2022-04-01 10:18:21 +0700

    todo service

---
+++ service/todo_service.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"github.com/teten-nugraha/go-dev-forum/models"
+	"github.com/teten-nugraha/go-dev-forum/repository"
+)
+
+func LoadTodos() []models.Todo {
+
+	todos := repository.FetchTodos()
+
+	return todos
+
+}
+
+func CreateTodo(newTodo models.Todo) models.Todo {
+
+	todo := repository.CreateTodo(newTodo)
+
+	return todo
+
+}
+
+func UpdateTodo(todo models.Todo) models.Todo {
+
+	todoUpdated := repository.UpdateTodo(todo)
+
+	return todoUpdated
+}
+
+func SetFinishTodo(id uint64) models.Todo {
+
+	todoFinished := repository.SetFinish(id)
+
+	return todoFinished
+
+}
